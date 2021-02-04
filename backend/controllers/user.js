8b7d7d018f7e5288abcd74d7335536d2799c70aa
EL FAKIH Karim <elfakihkarim@hotmail.Fr>
2021-02-04 21:08:50 +0100

    cours 1

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
 
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/user");
 
 exports.signup = (req, res, next) => {
@@ -32,7 +34,9 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: "TOKEN",
+            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+              expiresIn: "24h",
+            }),
           });
         })
         .catch((error) => res.status(500).json({ error }));
