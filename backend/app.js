2f834bdfb5dd15bd5e58029624c95a475c636777
EL FAKIH Karim <elfakihkarim@hotmail.Fr>
2021-02-04 22:21:40 +0100

    pekocko init

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const stuffRoutes = require("./routes/stuff");
+const saucesRoutes = require("./routes/sauces");
 const userRoutes = require("./routes/user");
 
 mongoose
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use("/api/stuff", stuffRoutes);
+app.use("/api/sauces", saucesRoutes);
 app.use("/api/auth", userRoutes);
 
 module.exports = app;
